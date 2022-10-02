a07ca3ec649db070c75b4a4f1b8a1634faec088e
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-10-02 10:18:56 +0200

    #Day-1 SwiftUI Challenge Project1 part one, #How to create variables and constants

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+
+//changeble variable
+var name = "Ali"
+name = "Azimo"
+name = "Usmon"
+//not changebale
+let charcater = "Jacky"
+//character = "Elon"
+//charcater = "Mark"
+
+
