69e3392f569e7aaf82e75bea166975179b82c93a
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-10-02 10:44:23 +0200

    #Day-1 SwiftUI Challenge Swift part one, #How to create strings

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Cocoa
 
+/*
+ ----Day-1 How to create variables and constants-----
 var hello = "Welcome back again"
 
 //changeble variable
@@ -25,5 +27,26 @@ let managerName = "Jo"
 let teamLead = "Pascal"
 let seniorDeveloper = "Michal"
 let meaningOfLife = "How many roads mast a man walk down?"
+*/
+/*
+---Day-1 How to create strings---
+*/
+let actor = "Denzel Washington"
+let fileName = "paris.jpg"
+let result = "⭐️ You win! ⭐️"
 
+let quote = "Then he tapped a sogn saying \"Believe\" and walked away"
+//multilineString
+let movie = """
+A day in the life
+Apple engineer
+"""
+
+print(actor.count)
+let nameResult = result.count
+print(nameResult)
+print(result.uppercased())
+
+//hasPrefix and lets us know whether a string starts with some letters of our choosing:
+print(movie.hasPrefix("A day"))
 
