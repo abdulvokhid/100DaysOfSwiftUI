cce627588721ba3b6e4c5372c01359cb0fb0ac49
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-10-10 14:40:08 +0200

    #Day-5 SwiftUI Challenge Swift part five, #How to use switch statements to check multiple conditions

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -434,4 +434,57 @@ if transport == .airplane || transport == .helicopter{
     print("Lets stay at home")
 }
  */
+/*
+---Day-5 How to use switch statements to check multiple conditions---
+*/
 
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.sun
+switch forecast {
+case .sun:
+    print("It should be a nice day")
+case .rain:
+    print("Pack an umbrella")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School is cancelled")
+case .unknown:
+    print("Our forecast generator is broken!")
+}
+
+let place = "Metropolis"
+
+switch place {
+case "Gotham":
+    print("You are batman")
+case "Mega_City One":
+    print("You are Judge Dredder")
+case "Wakanda":
+    print("You are Black Panther")
+default:
+    print("Who are you ?") // checked one by one could not find provided city thats why we got default output!!!
+}
+
+// Fallthrough allows us to print each case of our switch method if we want!!!
+let day = 5
+print("My true love gave to me...")
+switch day {
+case 5:
+    print("5 golden rings")
+    fallthrough
+case 4:
+    print("4 calling birds")
+    fallthrough
+case 3:
+    print("3 French hense")
+    fallthrough
+case 2:
+    print("2 turtle doves")
+    fallthrough
+default:
+    print("A partridge in a pear tree")
+}
