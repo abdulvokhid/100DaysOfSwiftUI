c29644a6f1879996447881957fe744f11c0014de
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-10-12 17:12:45 +0200

    #Day-6 SwiftUI Challenge Swift part six, #Checkpoint 3 created more swift way solution

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -595,16 +595,30 @@ print(multiples)
  */
 /*
 ---Checkpoint-3 ---
-*/
-
+// Both ways works !!!
+// basic way
 for i in 1...100 {
-    if i % 3 == 0 {
-        print("Fizz")
-    } else if i % 5 == 0 {
-        print("Buzz")
-    } else if i % 3 == 0 && i % 5 == 0 {
+    if i % 3 == 0 && i % 5 == 0 {
         print("FizzBuzz")
+    } else if i % 3 == 0 {
+        print("Fizz")
+    } else if i % 5 == 0{
+        print("Buzz")
     } else {
         print(i)
     }
 }
+// more swift way
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("FizzBuzz")
+    } else if i.isMultiple(of: 3) {
+        print("Fizz")
+    } else if i.isMultiple(of: 5) {
+        print("Buzz")
+    } else {
+        print(i)
+    }
+}
+ -----------------------------------------------------------------------------------------------
+*/
