21382b58fe36ef0b37ac867113444fec9cc56776
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-10-15 11:48:16 +0200

    #Day-7 SwiftUI Challenge Swift part seven, #How to customize parameter labels

--- Swift.playground/Contents.swift
+++ Swift.playground/Contents.swift
@@ -696,4 +696,39 @@ func gg() -> (name: String, name2: String) {
 let azim = gg()
 print("Name: \(azim.name) \(azim.name2)")
 */
+/*
+---Day-7 How to customize parameter labels---
+*/
 
+func rollDice(sides: Int, count: Int) -> [Int] {
+    var rolls = [Int]()
+    
+    for _ in 1...count {
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+    return rolls
+}
+let rolls = rollDice(sides: 6, count: 4)
+
+
+//This method of naming parameters for external use is so important to Swift that it actually uses the names when it comes to figuring out which method to call. This is quite unlike many other languages, but this is perfect valid in Swift:
+func hireEmployee(name: String) { }
+func hireEmployee(title: String) { }
+func hireEmployee(location: String) { }
+
+let lyric = "I see a red door and I want it painted black"
+print(lyric.hasPrefix("I see"))
+
+func isUppercase(_ string: String) -> Bool {
+    string == string.uppercased()
+}
+let string = "HELLO, WORLD"
+let result = isUppercase(string)
+
+func printTimesTable(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+printTimesTable(for: 2)
