793fd02858badc683a289aef8ce696133898aebc
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-12-18 13:10:59 +0100

    #Day-17 Challenge Project-1 part two:  Reading text from the user with TextField

--- WeSplitApp/WeSplitApp/ContentView.swift
+++ WeSplitApp/WeSplitApp/ContentView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        NavigationView{
-            Form{
-                Section {
-                    Text("Hello, world!")
-                }
+        Form {
+            Section{
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
             }
-            .navigationTitle("SwiftUI")
-            .navigationBarTitleDisplayMode(.inline)
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
         }
     }
 }
