c115daa8bbf7d395f14c33e46a9bc9cfb9801760
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-12-19 13:12:28 +0100

    #Day-17 SwiftUI Project-1 part two, calculating the total person

--- WeSplitApp/WeSplitApp/ContentView.swift
+++ WeSplitApp/WeSplitApp/ContentView.swift
@@ -14,6 +14,17 @@ struct ContentView: View {
     @State private var tipPercentage = 20
     
     let tipPercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double {
+        //calculation the total per person here
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView{
@@ -39,7 +50,7 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
