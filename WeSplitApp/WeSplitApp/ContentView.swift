b846e5ac5002c2b2383b44736d8900f75a0ac466
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-12-19 14:13:23 +0100

    Day-17 SwiftUI Project-1 part two, Hiding the keyboard

--- WeSplitApp/WeSplitApp/ContentView.swift
+++ WeSplitApp/WeSplitApp/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     var totalPerPerson: Double {
@@ -32,6 +33,7 @@ struct ContentView: View {
                 Section{
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people ")
@@ -54,6 +56,15 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
