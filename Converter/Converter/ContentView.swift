cb9c22159d2beff21f73652d2c73dfd0ddb98d56
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-12-21 16:44:48 +0100

    #Day-19 SwiftUI Challenge-1, setting up unit conversion app

---
+++ Converter/Converter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by Abdulvokhid Azimov on 21.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Text("Lets start")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Converter/Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Abdulvokhid Azimov on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
