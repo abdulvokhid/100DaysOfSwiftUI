841568995fd4a1a8d4f8f6923cb02f764896af40
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2022-12-31 12:20:17 +0100

    #Day-21 SwiftUI Challenge Project-2 part two, added assets in asset catalog and created stacks, vars and forEach loop

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,17 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) {}
-            Button("Cancel", role: .cancel) {}
-        } message: {
-            Text("Please read this")
+        ZStack {
+            Color.mint
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                }
+                ForEach(0..<3) {
+                    number in Button {
+                        //flag was tapped
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                    }
+                }
+            }
         }
     }
 }
