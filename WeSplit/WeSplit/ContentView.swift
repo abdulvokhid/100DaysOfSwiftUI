4a5b8b5085d747be8f8cdba827f52ec5805cdbe1
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-03-06 17:38:49 +0100

    #Project-1 part two SwiftUI ->  Reading text from the user with TextField

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
         NavigationStack{
             Form{
-                TextField("Enter something", text: $name)
-                Text("Your name is \(name)")
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
             .navigationTitle("WeSplit")
             .navigationBarTitleDisplayMode(.inline)
