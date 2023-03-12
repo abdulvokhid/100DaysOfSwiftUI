bba1905be1685a2b0cb2be8fd17ef50e50d3063b
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-03-12 10:36:46 +0100

    #Challenge-1 SwiftUI, added navigationStack, Form, Section and private vars

--- Challenge-1/Challenge-1/ContentView.swift
+++ Challenge-1/Challenge-1/ContentView.swift
@@ -7,14 +7,63 @@
 
 import SwiftUI
 
+//Time conversions: User choose seconds, minutes, hours or days
 struct ContentView: View {
+    
+    @State private var hour = 0.0
+    @State private var seconds = 0.0
+    @State private var minutes = 0.0
+    @State private var days = 0.0
+    @State private var timer = "seconds"
+    //Hiding the key
+    @FocusState private var inputIsFocused: Bool
+    
+    //options to choose for picker style
+    let times = ["seconds", "minutes", "days"]
+    
     var body: some View {
         NavigationStack {
             Form{
+                //deals with hours
                 Section {
-                    Text("Hello")
+                    TextField("Amounts", value: $hour, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                    Picker("Choose volue for output", selection: $timer) {
+                        ForEach(times, id: \.self) {
+                            Text($0, format: .number)
+                        }
+                    }.pickerStyle(.navigationLink)
                 }
-            }.navigationTitle("VolumeConversion")
+                //output for seconds
+                Section{
+                    Text("Seconds")
+                } header: {
+                    Text("Hours converted to Seconds")
+                }
+                //output for minutes
+                Section{
+                    Text("Minutes")
+                } header: {
+                    Text("Hours converted to Minutes")
+                }
+                //output for days
+                Section{
+                    Text("Days")
+                } header: {
+                    Text("Hours converted to Days")
+                }
+            }
+            .navigationTitle("TimeConversion")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done"){
+                        inputIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
