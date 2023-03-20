762f2c1a7a7aeeef75de9384da127a96c5e68153
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-03-20 17:17:06 +0100

    #Project-2 SwiftUI, setting up project

---
+++ Guess the Flag/Guess the Flag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Guess the Flag
+//
+//  Created by Abdulvokhid Azimov on 20.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess the Flag/Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Abdulvokhid Azimov on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
