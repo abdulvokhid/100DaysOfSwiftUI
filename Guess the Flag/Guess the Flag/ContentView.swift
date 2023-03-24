4bcc345aca466737efc21bf0114b9ab440ba185a
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-03-24 11:19:49 +0100

    #Project-2 SwiftUI part three, added state property to store users score

--- Guess the Flag/Guess the Flag/ContentView.swift
+++ Guess the Flag/Guess the Flag/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var userScore  = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled() //shuffled take cares of randomizing flags
    @State private var correctAnswer = Int.random(in: 0...2)
@@ -56,7 +57,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(userScore)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -72,8 +73,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            userScore += 1
         } else {
             scoreTitle = "Wrong"
+            userScore -= 1
         }
         showingScore = true
     }
