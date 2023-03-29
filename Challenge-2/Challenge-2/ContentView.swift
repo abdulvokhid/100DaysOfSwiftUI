ae208655ca16482af79b23312bbf0a5ae55dddb1
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-03-29 12:24:31 +0200

    #Challenge-2 SwiftUI, part two, added State and if statement

--- Challenge-2/Challenge-2/ContentView.swift
+++ Challenge-2/Challenge-2/ContentView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var options = ["🪨", "📃", "✂️"]
+    @State private var needToWin = Bool.random() // returns true or false about wether the player needs to win
+    @State private var rounds = 0 //number of game raunds
+    
+    let computerNo = Int.random(in: 0...2) //choose a random number between 0 abd 2 that will later be the computer game move
+    var toWin: String {
+        if options[computerNo] == "🪨" {
+            return "📃"
+        } else if options[computerNo] == "📃" {
+            return "✂️"
+        } else {
+            return "🪨"
+        } //returns the winning answer
+    }
+    
+       
     var body: some View {
-        VStack {
-            Text("Hello, world!")
+        ZStack {
+            Color.gray
+            VStack{
+                Text("Roc Paper Scissors")
+                    .padding()
+                    .scaledToFit()
+                    .font(.largeTitle)
+            }
         }
-        .padding()
     }
 }
 
