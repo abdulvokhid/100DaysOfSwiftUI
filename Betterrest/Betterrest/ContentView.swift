8931e9c604e8ff3c8123f9d314d4087b0d57658c
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-03 12:25:59 +0200

    #BetterRest SwiftUI part one, working with dates

--- Betterrest/Betterrest/ContentView.swift
+++ Betterrest/Betterrest/ContentView.swift
@@ -9,22 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
-    
     var body: some View {
         
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        
-        DatePicker("", selection: $wakeUp, in: Date.now...)
-            .labelsHidden()
-    }
-    
-    func exampleDate() {
-        //create a second Date instance set to one day in seconds from now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        //create a range from those two
-        let range = Date.now...tomorrow
+        Text(Date.now.formatted(date: .long, time: .shortened))
     }
 }
 
