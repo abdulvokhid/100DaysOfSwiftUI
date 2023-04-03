684013a60022eb119e1350e26f1186bdca889138
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-03 11:52:52 +0200

    #BetterRest SwiftUI part one, settingUP project

---
+++ Betterrest/Betterrest/BetterrestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterrestApp.swift
+//  Betterrest
+//
+//  Created by Abdulvokhid Azimov on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterrestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Betterrest/Betterrest/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Betterrest
+//
+//  Created by Abdulvokhid Azimov on 03.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Text("👋🏻")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
