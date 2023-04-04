11144667beab993ff736cdb34913c90415f54017
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-04 17:25:25 +0200

    #BetterRest SwiftUI part two, UI modified

--- Betterrest/Betterrest/ContentView.swift
+++ Betterrest/Betterrest/ContentView.swift
@@ -18,6 +18,14 @@ struct ContentView: View {
             VStack {
                 Text("When do you want to wake up")
                     .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
                 Text("Daily coffee intake")
                     .font(.headline)
                 Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
@@ -26,12 +34,9 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
-            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                .labelsHidden()
             
-            Text("Desired amount of sleep")
-                .font(.headline)
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            
             
         }
     }
