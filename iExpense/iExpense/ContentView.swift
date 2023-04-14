0f8d78eca9acf4c505e7c86fe419c545ce732bab
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-14 17:23:14 +0200

    #iExpense SwiftUI part one, project settings

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Abdulvokhid Azimov on 14.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
