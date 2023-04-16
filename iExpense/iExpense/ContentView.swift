d37f1c93356ce6365dca3c7f151d353f5696afbb
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-16 12:38:30 +0200

    #iExpense SwiftUI entry, Why @State only works with structs

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 
+class User {
+    var firstName = "Bilbo"
+    var lastname = "Baggins"
+}
+
 struct ContentView: View {
+    
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Text("Hello World")
+            Text("Hello, \(user.firstName) \(user.lastname)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastname)
+            
         }
     }
 }
