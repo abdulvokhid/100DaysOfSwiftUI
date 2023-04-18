5bf8983297ad38d8dfaaad115f2f7b8553fbbe63
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-18 13:15:49 +0200

    #iExpense SwiftUI part one, Building a list we can delete from

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -9,25 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     
-    struct User: Codable {
-        let firstName: String
-        let lastName: String
-    }
-    @State private var user = User(firstName: "Usman", lastName: "Hafiz")
+    @StateObject var expenses = Expenses()
     
     var body: some View {
         NavigationView {
-            VStack {
-                Button("Save User"){
-                    let encoder = JSONEncoder()
-                    
-                    if let data = try? encoder.encode(user){
-                        UserDefaults.standard.set(data, forKey: "UserData")
-                    }
+            List {
+                ForEach(expenses.items, id: \.name) {
+                    item in Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }label: {
+                    Image(systemName: "plus")
                 }
             }
-            .navigationTitle("Codable")
         }
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
---
+++ iExpense/iExpense/Expenseltem.swift
@@ -0,0 +1,14 @@
+//
+//  Expenseltem.swift
+//  iExpense
+//
+//  Created by Abdulvokhid Azimov on 18.04.2023.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
