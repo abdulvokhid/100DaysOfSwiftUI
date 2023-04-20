3ad47e988f9e19c6c57902e7bfc4434bbe623dca
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-20 13:30:46 +0200

    #iExpense SwiftUI part two, Final polish

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AddView: View {
     
     @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
     
     @State private var name = ""
     @State private var type = "Personal"
@@ -17,21 +18,24 @@ struct AddView: View {
     
     let types = ["Business", "Personal"]
     var body: some View {
-        Form {
-            TextField("Name", text: $name)
-            Picker("Type", selection: $type) {
-                ForEach(types, id: \.self) {
-                    Text($0)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
                 }
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
             }
-            TextField("Amount", value: $amount, format: .currency(code: "USD"))
-                .keyboardType(.decimalPad)
-        }
-        .navigationTitle("Add new expense")
-        .toolbar {
-            Button("Save") {
-                let item = ExpenseItem(name: name, type: type, amount: amount)
-                expenses.items.append(item)
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
             }
         }
     }
