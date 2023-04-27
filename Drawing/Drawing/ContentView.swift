9429e35142385d7fbc0d822cca424bf0cfb6e207
abdulvokhid <abdulvokhid.azimov97@gmail.com>
2023-04-27 13:20:10 +0200

    #Drawing SwiftUI part one, project settingUp

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Abdulvokhid Azimov on 27.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Drawing")
+        }
+        .navigationTitle("Drawing")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Abdulvokhid Azimov on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
